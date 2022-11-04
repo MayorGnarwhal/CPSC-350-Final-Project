f2583a44bbcd6d7c1d61b00776ebfa0351f96945
Harris Fort <harrisfort99@gmail.com>
2022-11-04 23:45:00 +0000

    Partials with javascript

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,8 +1,29 @@
-const links = document.querySelectorAll("li>a");
+// -- Insert into div
+const partialPath = "../partials/navigation.html";
 
-links.forEach(link => {
-    link.addEventListener("click", event => {
-        event.preventDefault();
-        console.log("click", link.textContent);
+async function fetchHtmlAsText(url) {
+    const response = await fetch(url);
+    return await response.text();
+}
+
+async function insertFileIntoDiv(div, filePath) {
+    div.innerHTML = await fetchHtmlAsText(filePath);
+}
+
+const navBarDiv = document.querySelector(".nav-bar");
+if (!navBarDiv) {
+    console.log("Navigation bar not found");
+} 
+else {
+    insertFileIntoDiv(navBarDiv, partialPath).then(function() {
+        // Handle links
+        const links = document.querySelectorAll(".nav-bar a");
+        
+        links.forEach(link => {
+            link.addEventListener("click", event => {
+                event.preventDefault();
+                console.log("click", link.textContent);
+            });
+        });
     });
-});
+}
