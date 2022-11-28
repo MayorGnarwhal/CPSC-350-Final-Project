6cf608c599bb1e68c54a62c0a6af02fa449665fe
Harris Fort <harrisfort99@gmail.com>
2022-11-28 15:37:33 +0000

    Basic client side error message

--- public/js/controllers/modals.js
+++ public/js/controllers/modals.js
@@ -3,25 +3,43 @@ var modals = {
         return document.querySelectorAll("[data-toggle=modal]");
     },
 
+    queryModals : function() {
+        return document.querySelectorAll(".modal");
+    },
+
     handleModalButton : function(button) {
         const modal = document.querySelector(button.getAttribute("data-target"));
-        const dismiss = modal.querySelector(".close");
 
         button.addEventListener("click", function() {
             modal.style.display = "block";
         });
-
-        if (dismiss) {
-            dismiss.addEventListener("click", function() {
-                modal.style.display = "none";
-            });
-        }
     },
 
-    handleAllModalButtons : function() {
+    handleAllModals : function() {
+        // handle modals
+        console.log(this.queryModals());
+        this.queryModals().forEach(modal => {
+            const dismiss = modal.querySelector(".close");
+
+            if (dismiss) {
+                dismiss.addEventListener("click", function() {
+                    modal.style.display = "none";
+                });
+            }
+        });
+
+        // handle modal toggle buttons
         this.queryModalButtons().forEach(button => {
             this.handleModalButton(button);
         });
+    },
+
+    errorModal : function(errorMessage) {
+        const modal = document.querySelector("#error-modal");
+        const label = modal.querySelector("#error-message");
+
+        label.textContent = "Error: " + errorMessage;
+        modal.style.display = "block";
     },
 }
 
