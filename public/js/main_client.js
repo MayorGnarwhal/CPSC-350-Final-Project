577542a2b785a287f026514ea1e6d1840e45cf12
Harris Fort <harrisfort99@gmail.com>
2022-11-30 19:55:13 +0000

    CSS cleanup

--- public/js/main_client.js
+++ public/js/main_client.js
@@ -2,4 +2,4 @@ import { partials } from "./controllers/partials.js";
 import { pages } from "./controllers/pages.js"
 
 await partials.populatePartial(document.querySelector("#nav-bar"));
-await pages.loadPage("profile");
+await pages.loadPage("index");
