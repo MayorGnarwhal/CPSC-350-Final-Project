3dd157e94b62fd6479ea4c17dc3e8de815e41348
xkk26x <karol2641@gmail.com>
2022-12-04 22:52:43 +0000

    test

--- public/js/main_client.js
+++ public/js/main_client.js
@@ -2,5 +2,5 @@ import { partials } from "./controllers/partials.js";
 import { pages } from "./controllers/pages.js"
 
 await partials.populatePartial(document.querySelector("#nav-bar"));
-await pages.loadPage("received_requests");
+await pages.loadPage("blocked_users");
 console.log("hello");
