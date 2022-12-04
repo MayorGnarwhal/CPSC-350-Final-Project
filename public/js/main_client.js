315872319b05137c02f5ce6642b3f6d75f8c5177
xkk26x <karol2641@gmail.com>
2022-12-04 22:50:36 +0000

    test

--- public/js/main_client.js
+++ public/js/main_client.js
@@ -2,5 +2,5 @@ import { partials } from "./controllers/partials.js";
 import { pages } from "./controllers/pages.js"
 
 await partials.populatePartial(document.querySelector("#nav-bar"));
-await pages.loadPage("sent_requests");
+await pages.loadPage("received_requests");
 console.log("hello");
