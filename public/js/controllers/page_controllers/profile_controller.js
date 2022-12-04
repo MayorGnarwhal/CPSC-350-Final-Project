0758600739505a28d731c1c7718760d318371c16
Harris Fort <harrisfort99@gmail.com>
2022-12-04 17:27:06 +0000

    Clean up for merge to main

--- public/js/controllers/page_controllers/profile_controller.js
+++ public/js/controllers/page_controllers/profile_controller.js
@@ -2,6 +2,7 @@ import { ajax } from "../../ajax";
 
 async function profileController(args) {
     var user = await ajax.fetchUser(args.user_id);
+    var localUser = await ajax.fetchUser();
 
     // populate page info
     document.querySelector("#user-name").textContent = user.first_name + " " + user.last_name;
@@ -10,6 +11,18 @@ async function profileController(args) {
     document.querySelector("#profile-picture").src = user.profile_picture.slice(7);
     document.querySelector("#friend-count").textContent = "13 friends";
     document.querySelector("#hidden_user_input").value = user.user_id;
+
+    // user options
+    const localUserOptions = document.querySelector("#local-user-options");
+    const friendUserOptions = document.querySelector("#friend-user-options");
+
+    if (user.user_id === localUser.user_id) {
+        localUserOptions.classList.remove("hidden");
+    }
+    else {
+        friendUserOptions.querySelector("input[type='hidden']").value = user.user_id;
+        friendUserOptions.classList.remove("hidden");
+    }
 
 
     // handle update modal
