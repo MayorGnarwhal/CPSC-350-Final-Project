452093bc5d0080b6a322ce09740fa59ce3a88254
xkk26x <karol2641@gmail.com>
2022-12-04 22:37:55 +0000

    test

--- public/js/main_client.js
+++ public/js/main_client.js
@@ -2,5 +2,5 @@ import { partials } from "./controllers/partials.js";
 import { pages } from "./controllers/pages.js"
 
 await partials.populatePartial(document.querySelector("#nav-bar"));
-await pages.loadPage("friends_list");
+await pages.loadPage("my_groups");
 console.log("hello");
