744ff6f847fa1ce670e3fb67715eb8737cfb3dee
Harris Fort <harrisfort99@gmail.com>
2022-12-05 02:59:48 +0000

    Cleanup for master

--- resources/controllers/initial_request.js
+++ resources/controllers/initial_request.js
@@ -13,8 +13,8 @@ var init = {
             response_handler.endResponse(response, '{"page": "login"}', 200);
         }
         else {
-            // response_handler.endResponse(response, '{"page": "index"}', 200);
-            response_handler.endResponse(response, '{"page": "groups"}', 200);
+            response_handler.endResponse(response, '{"page": "index"}', 200);
+            // response_handler.endResponse(response, '{"page": "groups"}', 200);
         }
     }
 };
