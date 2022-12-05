71afadf2b7d955d4bf3cc5db6a56b6368a50ba0f
Harris Fort <harrisfort99@gmail.com>
2022-12-05 15:04:42 +0000

    Improve typeahead search

--- resources/controllers/typeahead_results.js
+++ resources/controllers/typeahead_results.js
@@ -7,28 +7,28 @@ var typeahead = {
         "search_term": "required|string",
     },
 
-    // TODO: split search terms?
-    //   does not show results if space in words
     func : async function(body, response) {
-        const searchTerm = body.search_term.trim();
+        // get all words longer than 2 characters
+        const terms = body.search_term.trim().split(" ").filter(term => term.length >= 2);
         
-        if (searchTerm.length < 3) {
+        if (terms.length === 0) {
             response_handler.endResponse(response, undefined, 201);
         }
 
-        database.query(
-            `${queries.USER} 
-            WHERE first_name LIKE '%%${searchTerm}%%'
-            OR last_name LIKE '%%${searchTerm}%%'
-            LIMIT 5
-        `, function(error, users) {
+        var whereClause = "WHERE";
+        terms.forEach(term => {
+            whereClause += ` first_name LIKE '%%${term}%%' OR last_name LIKE '%%${term}%%' OR`
+        });
+        whereClause = whereClause.slice(0, -3);
+
+        database.query(`${queries.USER} ${whereClause} LIMIT 5`, function(error, users) {
             if (error) {
                 response_handler.errorResponse(response, `DB ERROR: ${error}`);
             }
             else {
                 response_handler.endResponse(response, JSON.stringify(users), 201);
             }
-        })
+        });
     }
 };
 
